45f080899ea74d9b84065fe452a0e915e55fb8d1
andeisaldyun <asaldyun@gmail.by>
2021-12-27 17:53:10 +0300

    v0

--- main.go
+++ main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo"
-	"helloworld/config"
 	"net/http"
 )
 
 func main() {
-	postgresConfig := config.NewPostgresConfig()
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, fmt.Sprintf("Hello,  %s", postgresConfig.DbName))
+		return c.String(http.StatusOK, fmt.Sprintf("Hello,  %s", "test"))
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
